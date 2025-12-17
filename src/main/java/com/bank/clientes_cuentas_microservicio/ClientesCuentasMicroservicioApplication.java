cac7833ac34b6852d953bae1768be47ed2f7d8de
juancouselo <couseloinvestor@gmail.com>
2025-12-17 07:25:31 +0100

    chore: initialize project with Spring Initializr

---
+++ src/main/java/com/bank/clientes_cuentas_microservicio/ClientesCuentasMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.bank.clientes_cuentas_microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesCuentasMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesCuentasMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/clientes_cuentas_microservicio/ClientesCuentasMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.clientes_cuentas_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesCuentasMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
